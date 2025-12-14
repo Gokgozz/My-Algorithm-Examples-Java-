a7edd8207e42a5f06a354e170b97e7feadce654c
Süleyman Gökgöz <111862288+Gokgozz@users.noreply.github.com>
2025-12-14 20:06:12 +0300

    Update C09_FizikHizBulma.java

--- C09_FizikHizBulma.java
+++ C09_FizikHizBulma.java
@@ -1,5 +1,4 @@
-//fizikteki bir problemin hesaplamasini yap ( kinetik enerji , momentum , potansiyel enerji vb ) dizi kullanma sarti ile.
-//ben hız hesaplayacağım v = v0 ​+ a⋅t
+// hız hesaplayacağım v = v0 ​+ a⋅t
 package A06_diziler_arrays;
 
 import java.util.Scanner;
@@ -9,17 +8,17 @@ public class C09_FizikHizBulma {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("ilk hizi giriniz (V0) : ");
-        double startingSpeed = input.nextDouble();
-        
-        System.out.print("ivmeyi giriniz (a) : ");
+        System.out.print("Zamani giriniz (saniye,s) : ");
+        double time = input.nextDouble();
+
+        System.out.print("ivmeyi giriniz (m/(s*s)) : ");
         double acceleration = input.nextDouble();   //acceleration ingilizcede ivme demek
 
-        System.out.print("Zamani giriniz (t) : ");
-        double time = input.nextDouble();
-        
-        double speed = startingSpeed + (acceleration*time);
-        System.out.print("Hiziniz : " + speed);
+        System.out.print("ilk hizi giriniz (m/s) : ");
+        double startingSpeed = input.nextDouble();
+
+        double speed = startingSpeed + (acceleration * time);
+        System.out.print("Son hiziniz: " + speed + " m/s");
 
     }
 
